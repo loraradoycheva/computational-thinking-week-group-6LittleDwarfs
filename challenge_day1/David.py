077618fb68e1ca9574f21b8da3659e1b7c5a89bb
dvaud <141523348+dvaud@users.noreply.github.com>
2026-08-31 12:38:54 +0200

    First David part

--- challenge_day1/David.py
+++ challenge_day1/David.py
@@ -1,3 +1,10 @@
+import Bao
+import Tobit
+import David
+import Lora
+import Rajko
+import Tarling
+
 def give_david():
     return "David Rahimi"
 
@@ -5,7 +12,7 @@ def char():
     return "Vuk"
 
 def act1par5():
-    return "Valami"
+    return f"They usualy dont go this deep in the forest. Legend says, a terribel whitch lives in a hut and turns lost wanderers into rabbits. '{Tarling.char()} stop!' shouted {char()} as he run after {Tarling.char()}. As the screams ekoed the woods, all the figting stopped as {Tobit.char()}, {Bao.char()}, {Lora.char()} and {Rajko.char()} watched whit a worryd gaze. Should we go after then?"
 
 def act2par5():
     return "Valami"
