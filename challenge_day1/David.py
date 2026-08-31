6c7a283eaabe0de4153b8011fe64b07c4cc33245
tarling <tarling.gorgun@student.uva.nl>
2026-08-31 14:46:08 +0200

    Fix David story paragraph

--- challenge_day1/David.py
+++ challenge_day1/David.py
@@ -12,10 +12,10 @@ def char():
     return "Vuk"
 
 def act1par3():
-    return f"They usualy don't go this deep in the forest. Legend says, a terrible witch lives in a hut and turns lost wanderers into rabbits. '{Tarling.char()} stop!' shouted {char()} as he ran after {Tarling.char()}. As the screams echoed the woods, all the fighting stopped. {Tobit.char()}, {Bao.char()}, {Lora.char()} and {Rajko.char()} watched with a worried gaze. Should we go after then?"
+    return f"They usually don't go this deep in the forest. Legend says, a terrible witch lives in a hut and turns lost wanderers into rabbits. '{Tarling.char()} stop!' shouted {char()} as he ran after {Tarling.char()}. As the screams echoed through the woods, all the fighting stopped. {Tobit.char()}, {Bao.char()}, {Lora.char()} and {Rajko.char()} watched with a worried gaze. Should we go after them?"
 
 def act2par1():
-    return "{Tarling.char()} and {char()} starrted laughing as the others gleefully walked into their trap. If {Bao.char()}, {Tobit.char()}, {Lora.char()} and {Rajko.char()} visit the witch, theiy will be turned into rabits. Rabbits the secret wolfs can eat for dinner tonight. YUM!"
+    return f"{Tarling.char()} and {char()} started laughing as the others gleefully walked into their trap. If {Bao.char()}, {Tobit.char()}, {Lora.char()} and {Rajko.char()} visit the witch, they will be turned into rabbits. The secret wolves can eat rabbits for dinner tonight. YUM!"
 
 def act3par3():
     return "Valami"
