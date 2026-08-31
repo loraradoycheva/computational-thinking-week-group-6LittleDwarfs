ba4a3cc071f82c2072a8bf5aa968b3f21ce5d185
Huy Bao Nguyen <jimmyhuybao@gmail.com>
2026-08-31 11:52:39 +0200

    fixed Bao functions for story

--- challenge_day1/Bao.py
+++ challenge_day1/Bao.py
@@ -1,2 +1,13 @@
 def give_bao():
     return "Huy Bao Nguyen"
+def char():
+    return "Edgar Allan Poe"
+
+def act1par3():
+    return "Iets" 
+
+def act2par3():
+    return "Iets" 
+
+def act3par3():
+    return "Iets" 
