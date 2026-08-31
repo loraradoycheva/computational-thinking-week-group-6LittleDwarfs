7f2819d27e48f422977adf78966467d2de92719b
dvaud <141523348+dvaud@users.noreply.github.com>
2026-08-31 11:58:05 +0200

    Initial story

--- challenge_day1/David.py
+++ challenge_day1/David.py
@@ -1,2 +1,14 @@
 def give_david():
     return "David Rahimi"
+
+def char():
+    return "Kisvakond"
+
+def act1par5():
+    return "Valami"
+
+def act2par5():
+    return "Valami"
+
+def act3par5():
+    return "Valami"
