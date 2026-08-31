d7d8c1be60e02a0e34d2e92c11370658a4650286
Huy Bao Nguyen <jimmyhuybao@gmail.com>
2026-08-31 11:07:27 +0200

    Made git_exercise.py get team members' names

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,1 +1,17 @@
+import Bao
+import Tobit
+import hello_by_david
+import hello_by_lora
+import Rajko
+import Tarling
 
+def get_team_members():
+    print("This is Team 6 Little Dwarfs. We are:")
+    print(Bao.get_name())
+    print(Tobit.give_tobit())
+    print(hello_by_david.get_name())
+    print(hello_by_lora.get_name())
+    print(Rajko.give_rajko())
+    print(Tarling.give_tarling())
+
+print(get_team_members())
