fe6a9dc68f6539532b666ceb9cd976fe7c733aa1
Fenjor <rajkos2005@gmail.com>
2026-08-31 11:43:06 +0200

    bonus

--- challenge_day1/Rajko.py
+++ challenge_day1/Rajko.py
@@ -1,2 +1,14 @@
 def give_rajko():
     return "Rajko Scherpenzeel"
+
+def char():
+    return "Dovstoevsky"
+
+def act1par1():
+    return "Iets" 
+
+def act2par1():
+    return "Iets" 
+
+def act3par1():
+    return "Iets" 
