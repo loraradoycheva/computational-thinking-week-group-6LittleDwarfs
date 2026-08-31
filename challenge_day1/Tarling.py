c07fc219c21f3326b7e9ed484254d8570e3c598a
tarling <tarling.gorgun@student.uva.nl>
2026-08-31 13:46:45 +0200

    Add Tarling story paragraphs

--- challenge_day1/Tarling.py
+++ challenge_day1/Tarling.py
@@ -5,10 +5,10 @@ def char():
     return "Nijntje"
 
 def act1par6():
-    return "Nijntje: I am Nijntje, the little white rabbit. I love to play and explore the world around me."
+    return "Nijntje: I am Nijntje, the little white rabbit. I love to play and explore the world around me. My best friend is Gryla and we like to travel together. However we don't have enough money which is why we usually go to the park with our other friends Fritz, Ieniemienie, Moomin and Vuk. We callourseles the 6 Little dwarfs because we are all short."
 
 def act2par6():
-    return "Nijntje: Today, I found a beautiful garden full of colorful flowers. I can't wait to show it to my friends!"
+    return "Nijntje: Today, I found a beautiful garden full of colorful flowers. I can't wait to show it to my friends!. I think Vuk would love this because he has a little crush on Moomin and wanted to ask her out on a date. Ieniemienie and Fritz has been giving him tips on how to plan the perfect date but Gryla thinks most the ideas soud stupd which s true. For example one of te ide ws to take her sky diving. I thouh that was crazy because she has a fer of hight. "
 
 def act3par6():
-    return "Nijntje: This is the most beautiful garden I have ever seen. I am so happy to be here with my friends!"
+    return "Nijntje: This isthe most beautiful garden I have ever seen. I m so happy that we cn all be togeter here but i wish Vuk was also here said Ieniemienie. Moomin asked why isnt he her?. Because Fritz and vuk are beefing over beef said Gryla. Nijnta screem: IT WS SO STUPD ARGUMNT, Vuk want t eat prok for dinner but Fritz decide he wanted to became muslim and eveyrone shoul stop eating. pork because its haram. So they were fighting about it and hnostlt idk why someone would forc a religion on anyone but we should all jus respect eveyone wishes and eat what we wanna eat like cmon man. Moomin mayb you can try to talk to themm and figure this out. Moomin replied: i wll try my best we need to just all be friends and nt argue about stipid sdtuff like this. "
