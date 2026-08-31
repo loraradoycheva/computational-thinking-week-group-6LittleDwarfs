e36542df4253134d0cbb905e7392a574993c0f1a
Lora Radoycheva <loraradoycheva184@gmail.com>
2026-08-31 13:23:42 +0200

    story paragraph

--- challenge_day1/Lora.py
+++ challenge_day1/Lora.py
@@ -1,11 +1,17 @@
+import Bao
+import Tobit
+import David
+import Rajko
+import Tarling
+
 def give_lora():
     return "Lora"
 
 def char():
     return "Moomin"
 
-def act1par5():
-    return "Some text" 
+def act1par4():
+    return f"{Bao.char()} pulled a Circe move (Odyssey reference intended), then {Tobit.char()}, {Rajko.char()} and {char()} turned int rabbits. The whitch was very hapy, she had a new meal for dinne. However, {Tarling.char()} and {David.char()} were stil humans in the woos. They were very sared, but they had to find a way to save their friends. They ran after the whitch, but she was too fast for them. They had to think of a plan."
 
 def act2par4():
     return "Some text" 
