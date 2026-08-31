6ce71fd39e7973ebe267ed2d4f67529175592229
tarling <tarling.gorgun@student.uva.nl>
2026-08-31 14:34:51 +0200

    Fix Bao story paragraph

--- challenge_day1/Bao.py
+++ challenge_day1/Bao.py
@@ -7,14 +7,11 @@ import Tarling
 
 def give_bao():
     return "Huy Bao Nguyen"
+
 def char():
     return "Gryla of the woods"
 
 def act1par4():
     return f"'No, we should go deeper into the forest because any lone wanderer will be brought back to the witch's hut and turned into a rabbit.' said {char()}. 'If we want to save them, we have to go deeper.' {Tobit.char()}, {char()}, {Lora.char()} and {Rajko.char()}, although reluctantly, pressed on. After a while, they encountered the hut of the witch. 'We have to go in now!' said {char()}. 'No, we will be turned into a rabbit too!' shouted {Tobit.char()}. {char()} shouted back: 'Who will save them if we don'?!' and went in anyway. What {Tobit.char()}, {Lora.char()} and {Rajko.char()} didn't know is that {char()} is actually THE WITCH and lured them to her hutttt!"
-
-def act2par4():
-    return "Iets"
-
 def act3par4():
     return "Iets" 
