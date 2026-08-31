d3b2ff9c597c2bc8a0b64c7022002677f970570c
Fenjor <rajkos2005@gmail.com>
2026-08-31 11:45:48 +0200

    tobit functions

--- challenge_day1/Tobit.py
+++ challenge_day1/Tobit.py
@@ -1,2 +1,14 @@
 def give_tobit():
     return "Tobit Kayser"
+
+def char():
+    return "Fritz"
+
+def act1par2():
+    return "Etwas" 
+
+def act2par2():
+    return "Etwas" 
+
+def act3par2():
+    return "Etwas" 
