17c18bd49ab2dc0788d0fb4b8b9842d97ae8afa9
Huy Bao Nguyen <jimmyhuybao@gmail.com>
2026-08-31 12:11:59 +0200

    tarling functions

--- challenge_day1/Tarling.py
+++ challenge_day1/Tarling.py
@@ -3,3 +3,12 @@ def give_tarling():
 
 def char():
     return "Nijntje"
+
+def act1par6():
+    return "Nijntje: I am Nijntje, the little white rabbit. I love to play and explore the world around me."
+
+def act2par6():
+    return "Nijntje: Today, I found a beautiful garden full of colorful flowers. I can't wait to show it to my friends!"
+
+def act3par6():
+    return "Nijntje: This is the most beautiful garden I have ever seen. I am so happy to be here with my friends!"
