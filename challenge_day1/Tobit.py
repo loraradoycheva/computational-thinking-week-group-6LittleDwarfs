53b125a400852c1b749ba391279035bcb85fb19c
Fenjor <rajkos2005@gmail.com>
2026-08-31 12:27:08 +0200

    tobit act 1 par 2

--- challenge_day1/Tobit.py
+++ challenge_day1/Tobit.py
@@ -1,3 +1,10 @@
+import Bao
+import Tobit
+import David
+import Lora
+import Rajko
+import Tarling
+
 def give_tobit():
     return "Tobit Kayser"
 
@@ -5,7 +12,7 @@ def char():
     return "Fritz"
 
 def act1par2():
-    return "Valami" 
+    return f"On a beautiful Moonday morning they went on there regular morning stroll. All of the suddennn a frightening scream echoed through dee dark wounds. {Bao.char()} and {Lora.char()} quickly hid behind {Rajko.char()}. While the others weer stunlocked {David.char()} started hitttting {Tobit.char()}. \"Stop screaming like a maniac you fool. You are scaring us all. \". After a lot of back and fourth, {Tarling.char()} finally had enough and started to walk further inside the wouds. \n" 
 
 def act2par2():
     return "Valami" 
