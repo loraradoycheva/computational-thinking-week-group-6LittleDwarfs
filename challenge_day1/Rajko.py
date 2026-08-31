1d8b879fb6e4b1e8b193d76e25dd80bd5b8ec0a8
Fenjor <rajkos2005@gmail.com>
2026-08-31 14:11:12 +0200

    Act2 Par 2

--- challenge_day1/Rajko.py
+++ challenge_day1/Rajko.py
@@ -13,10 +13,8 @@ def char():
 
 def act1par1():
     return f"Once upon a time there was a group of characters called the Six Dwarves. Their names are {char()}, {Lora.char()}, {David.char()}, {Tobit.char()}, {Tarling.char()}, {Bao.char()}. Whut they didd not realisze however was that 2 of the dwarves were not dwerges at all but rather disguised wolves. This is the story of how they found out who si friend and who is foe." 
-
 def act2par1():
-    return "Iets" 
-
+    return f"The witches however do not want to contribute to {Tarling.char()} and {David.char()}’s plannes. Instead they want to drive out the wolfs who keep eating their rabbit friends, when {Tobit.char()} {Lora.char()} and {char()} get to the hut and the witches get them in their cages they try to find the straggling {Bao.char()}. “{Bao.char()} where are you?” they scream.  {David.char()} overhearinning this asks: “There’s a gorilla in the woods?”. This prompts {Tarling.char()} to give him a smakkerd on the nose and ask him to get his ackt together in order to be ready to catch the rabbits when the witches have transformed them."
 def act3par1():
     return "Iets" 
 
