cdb905afd87d5a9368640285dbbdb471b34b6756
Fenjor <rajkos2005@gmail.com>
2026-09-01 14:05:56 +0200

    Prime Number 4

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,1 +1,5 @@
-def solution_station4():
+def solution_station4(x): 
+    for i in range(2, x): #for every value between 1 and n
+        if x%i == 0: #check if i divides n
+            return False #if this is true, n is not prime
+    return True if x > 1 else False #values less than 2 are not prime
