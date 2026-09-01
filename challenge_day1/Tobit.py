d24600a373ad010883049cd6b2fde39dc6cbb328
Lora Radoycheva <loraradoycheva184@gmail.com>
2026-09-01 16:36:16 +0200

    act 2 par 3

--- challenge_day1/Tobit.py
+++ challenge_day1/Tobit.py
@@ -15,7 +15,7 @@ def act1par2():
     return f"On a beautiful Moonday morning they went on there regular morning stroll. All of the suddennn a frightening scream echoed through dee dark wounds. {Bao.char()} and {Lora.char()} quickly hid behind {Rajko.char()}. While the others weer stunlocked {David.char()} started hitttting {Tobit.char()}. \"Stop screaming like a maniac you fool. You are scaring us all. \". After a lot of back and fourth, {Tarling.char()} finally had enough and started to walk further inside the wouds. \n" 
 
 def act2par2():
-    return "Valami" 
+    return f"Alll of the suldden {Lora.char()} woke up. Alround them stood {Bao.char()}, {Rajko.char()}, {David.char()}, {Tobit.char()} and {Tarling.char()}. {Lora.char()} asked: 'What happened? Did I dream all of this?' To which {Bao.char()} answered: 'We were on our hike through the mountains and all of the sudden you collapes. Are you fine?' To which {Lora.char()} replied: 'So I did dream all of this nonsense. I think I am lcompletely fine. Let's take a rest andlthen get going again.'\n" 
 
 def act3par2():
     return "Valami" 
