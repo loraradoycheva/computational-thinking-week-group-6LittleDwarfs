c6c36b129d02ad23bddc6876f25a111b8b2c1d9a
Huy Bao Nguyen <jimmyhuybao@gmail.com>
2026-09-01 16:51:46 +0200

    Add Act 2 part 4

--- challenge_day1/Bao.py
+++ challenge_day1/Bao.py
@@ -15,7 +15,6 @@ def act1par4():
     return f"'No, we should go deeper into the forest because any lone wanderer will be brought back to the witch's hut and turned into a rabbit.' said {char()}. 'If we want to save them, we have to go deeper.' {Tobit.char()}, {char()}, {Lora.char()} and {Rajko.char()}, although reluctantly, pressed on. After a while, they encountered the hut of the witch. 'We have to go in now!' said {char()}. 'No, we will be turned into a rabbit too!' shouted {Tobit.char()}. {char()} shouted back: 'Who will save them if we don'?!' and went in anyway. What {Tobit.char()}, {Lora.char()} and {Rajko.char()} didn't know is that {char()} is actually THE WITCH and lured them to her hutttt!"
 
 def act2par4():
-    return f"hello"
-
+    return f"The 6 little Dwarfs went on wid they're journey and after a while them arrive at the city of Robots! 'Wow, this is amazing!' said {char()}. 'OMG there are robots fighting on the screen!!'{Lora.char()} exclaimed, pointing at the big screen of the tallest biulding. Then {Tobit.char()} saw a food stand with a sign that said: 'Free food for all!'. 'Let's go there!' said {Tobit.char()}. They walked to the food stand and ordered some food. But while eating, {Rajko.char()} started to feel sick and dizzy, then the friend group all started to feel dizzy. Then they all fell asleep and woke up in a dark room. 'Where are we?' asked {Lora.char()}. 'I don't know, but I think we are in a prison cell.'" 
 def act3par4():
     return "Iets" 
