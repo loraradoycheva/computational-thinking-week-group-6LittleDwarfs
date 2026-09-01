b91822053184ea9b221d1cc38044e6071fc32727
Fenjor <rajkos2005@gmail.com>
2026-09-01 14:30:41 +0200

    Fibbonacci and station6 fix

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,1 +1,14 @@
-def solution_station1():
+def solution_station1(index):
+    if index == 0:
+        return 0
+    elif index == 1:
+        return 1
+    
+    value_1 = 0
+    value_2 = 1
+    for i in range(1,index):
+        temp = value_1
+        value_1 = value_2
+        value_2 = temp + value_2
+
+    return value_2
--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -5,9 +5,9 @@ def solution_station6(input):
 
     x = np.array([0.6, 0.3, 2.8, 1.7, 1.4, 2.5, 2.2, 0, 2.6, 2.4, 2.9])
     y = np.array([0.5646, 0.2955, 0.335, 0.9917, 0.9854, 0.5985, 0.8085, 0, 0.5155, 0.6755, 0.2392])
-    p = Polynomial.fit(x, y, deg=4)
+    p = Polynomial.fit(x, y, deg=len(x)-1)
     print(p.convert()) 
     print(p(input))
 
 if __name__ == "__main__":
-    solution_station6(58)
+    solution_station6(0.7)
