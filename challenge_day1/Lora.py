eef41321f9edaa84364dc0d52a5dfce701546812
Lora Radoycheva <loraradoycheva184@gmail.com>
2026-09-01 16:16:15 +0200

    fix function calls

--- challenge_day1/Lora.py
+++ challenge_day1/Lora.py
@@ -10,11 +10,11 @@ def give_lora():
 def char():
     return "Moomin"
 
-def act1par4():
+def act1par5():
     return f"{Bao.char()} pulled a Circe move (Odyssey reference intended), then {Tobit.char()}, {Rajko.char()} and {char()} turned int rabbits. The whitch was very hapy, she had a new meal for dinne. However, {Tarling.char()} and {David.char()} were stil humans in the woos. They were very sared, but they had to find a way to save their friends. They ran after the whitch, but she was too fast for them. They had to think of a plan."
 
-def act2par4():
+def act2par5():
     return f"{Bao.char()}, {Tobit.char()}, {Rajko.char()}, {Tarling.char()}, {David.char()}, {char()}"
 
-def act3par4():
+def act3par5():
     return f"{Bao.char()}, {Tobit.char()}, {Rajko.char()}, {Tarling.char()}, {David.char()}, {char()}"
