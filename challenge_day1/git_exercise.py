1e245a0a22d217d35435c3b7da25ddb7ef8e4508
Fenjor <rajkos2005@gmail.com>
2026-09-01 14:33:54 +0200

    tester

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -24,9 +24,9 @@ def get_story():
     print(Tarling.act1par6())
 
     # Act 2
+    print(David.act2par1())
     print(Rajko.act2par2())
     print(Tobit.act2par2())
-    print(David.act2par3())
     print(Bao.act2par4())
     print(Lora.act2par5())
     print(Tarling.act2par6())
