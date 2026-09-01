1360effdf78db6bca377b8d3e7a9b5ffe299589a
ibao127 <jimmyhuybao@gmail.com>
2026-09-01 15:02:56 +0200

    Add _ to function name

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 
 DAYS_JA = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
 
-def solution_station2(d):
+def solution_station_2(d):
     if isinstance(d, str):
         d = datetime.strptime(d, "%Y-%m-%d").date()
     return DAYS_JA[d.weekday()]
