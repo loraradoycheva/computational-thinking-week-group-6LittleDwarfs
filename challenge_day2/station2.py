fe4d9cac4b32713bd0ac17643a698f736b8d37a0
Huy Bao Nguyen <jimmyhuybao@gmail.com>
2026-09-01 10:17:32 +0200

    Add station 2 function

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -2,10 +2,10 @@ from datetime import date, datetime
 
 DAYS_JA = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
 
-def weekday_ja(d):
+def solution_station2(d):
     if isinstance(d, str):
         d = datetime.strptime(d, "%Y-%m-%d").date()
     return DAYS_JA[d.weekday()]
 
-print(weekday_ja("2024-10-12"))   
-print(weekday_ja(date.today()))
+print(solution_station2("2024-10-12"))   
+print(solution_station2(date.today()))
