71f1176ca25375856b43a1f583fb1dbea887a8bd
Fenjor <rajkos2005@gmail.com>
2026-09-03 10:12:21 +0200

    Rust Challenge 5

---
+++ challenge_day4/Rust_step5/target/debug/deps/Rust_step5.d
@@ -0,0 +1,5 @@
+C:\Users\rajko\Documents\UVA\26-27\CAMP1\Dwerg\computational-thinking-week-group-6LittleDwarfs\challenge_day4\Rust_step5\target\debug\deps\Rust_step5.d: src\main.rs
+
+C:\Users\rajko\Documents\UVA\26-27\CAMP1\Dwerg\computational-thinking-week-group-6LittleDwarfs\challenge_day4\Rust_step5\target\debug\deps\Rust_step5.exe: src\main.rs
+
+src\main.rs:
