d36b43d5b12ecfab134939957e448816be0dfcfe
Lora Radoycheva <loraradoycheva184@gmail.com>
2026-09-03 11:03:33 +0200

    running step 3

--- challenge_day4/Julia_step3/sort.jl
+++ challenge_day4/Julia_step3/sort.jl
@@ -1,7 +1,7 @@
 using DataFrames, CSV, Statistics, DelimitedFiles
 
 # Read the CSV file into a DataFrame
-people_df = CSV.File("data3.csv") |> DataFrame
+people_df = CSV.File("../Go_step2/data3.csv") |> DataFrame
 
 # Function to classify a score based on quartiles
 function classify_score(score, quartiles)
